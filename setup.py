3c24fefa629b09b9c8287757f62797972ed764bc
nakish13 <78470594+nakish13@users.noreply.github.com>
2021-02-06 22:04:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import waitress
+port_= int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 model = pickle.load(open('iris_model.pkl', 'rb'))
@@ -20,3 +21,5 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True,use_reloader=False)
+    serve(app,port=port_)
+
