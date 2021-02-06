e9e1877b226e26daaa9cbfc0489d653bb39fbb96
ayush <ayushpatelnakish@gmail.com>
2021-02-06 16:25:31 +0530

    first commit

---
+++ routes.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('iris_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [np.array([int(x) for x in request.form.values()])]
+    prediction = model.predict(data)
+    
+    return render_template('index.html', prediction_text=f"Its a {prediction[0]}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,use_reloader=False)
