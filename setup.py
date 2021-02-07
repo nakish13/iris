cfa47e4f42f4a408804be38f64631a2aaf84afa4
nakish13 <78470594+nakish13@users.noreply.github.com>
2021-02-07 13:17:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import waitress
-port_= int(os.environ.get("PORT", 5000))
+
 
 app = Flask(__name__)
 model = pickle.load(open('iris_model.pkl', 'rb'))
